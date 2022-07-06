8b8f3b2044e7009ba6232fbbee5b8ad4fae4d151
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-07-06 12:23:32 +0300

    Add Task07 and Task08

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Алгоритм обхода директория - посмотреть все папки и файлы в папке и во вложенных папкаххи файлах -> тоже папки и файлы
+
+string path = "D:/Евгений/Программирование/Лекции/Закомство с языками программирования/recur/Task02";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo [] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);   
+}
---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Обход выражения: ((4-2)*(1+3)) / 10. Это бинарное дерево.
+
+string emp = string.Empty;
+string [] tree = {emp, "/","*","10","-","+",emp, emp,"4", "2","1","3" };
+//                0    1    2   3    4   5   6    7   8    9   10  11
+void InOrderTraversal(int pos = 1)
+{
+    if(pos < tree.Length)
+    {
+        int left = 2*pos;
+        int right = 2*pos + 1;
+        if(left < tree.Length && !string.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]); // позицию Console.WriteLine можно менять, от этого вывод буде разным.
+        if(right < tree.Length && !string.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+        
+    }
+}
+InOrderTraversal();
